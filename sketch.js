af0a2064611348fe2917f5170a99a02a67bb7677
angelawrong <46111705+angelawrong@users.noreply.github.com>
2018-12-25 09:29:06 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -48,9 +48,9 @@ for (var n=0; n<100 ; n++){
 function draw() {
 	
 	
-	//textSize(32);
-	//textAlign(CENTER);
-	//text("merry x'mas & happy 2019", 0, 0, 1080, 30);
+	textSize(32);
+	textAlign(CENTER);
+	text("merry x'mas & happy 2019", 0, 0, 1080, 30);
 
 	
 
