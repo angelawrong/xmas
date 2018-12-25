c82eaab6de277bdb1f52d1de7bd0a7164c8192e1
angelawrong <46111705+angelawrong@users.noreply.github.com>
2018-12-25 08:51:22 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,8 @@
-var //counter1 = 0;
-var //counter2 = 0
-var //counter3 = 0;
-var //counter4 = 0
-var //counter5 = 0;
+var counter1 = 0;
+var counter2 = 0;
+var counter3 = 0;
+var counter4 = 0;
+var counter5 = 0;
 
 
 
