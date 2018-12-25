1f81a99a42001f18685fbd3bb6a8f5f01cf43850
angelawrong <46111705+angelawrong@users.noreply.github.com>
2018-12-25 09:41:07 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,5 @@
-var counter1 = 0;
-var counter2 = 0
-var counter3 = 0;
-var counter4 = 0
-var counter5 = 0;
+
+
 
 
 
@@ -10,7 +7,6 @@ function mousePressed() {
 clear();
 background('#111111');
 blendMode(LIGHTEST);
-	
 
 for (var n=0; n<100 ; n++){
 	
@@ -30,10 +26,8 @@ function setup() {
   background('#111111');
   cursor(CROSS);
   frameRate(24);
-	
-
   
-for (var n=0; n<100 ; n++){
+   for (var n=0; n<100 ; n++){
 	
 	var gradient = color('hsba(225, 80%, 100%, ' + (100-n)/100 + ')');
 	stroke(gradient);
@@ -46,19 +40,24 @@ for (var n=0; n<100 ; n++){
 }
 
 function draw() {
+	//noStroke();
+	//fill('#8f98ff');
 	
+	 
 	
-	//textSize(32);
-	//textAlign(CENTER);
-	//text("merry x'mas & happy 2019", 0, 0, 1080, 30);
+	textSize(32);
+	textAlign(CENTER);
+text("merry x'mas & happy 2019 !", 0, 0, 1080, 30);
 
-	
+
+
 
 	var shadesGray = [66, 77, 88, 99];
 
 	var gray = random(shadesGray);
 	
 	var g = color('hsb(0, 0%, ' + gray + '%)');
+	
 	
 	stroke('#8f98ff');
 	strokeWeight(1);
@@ -83,48 +82,39 @@ function draw() {
 			var green = color('hsba(180, 60%, 100%, 0.25)');
 			
 			fill(green);
-			
-			counter1++;
+			//fill('#111111');
 			
 		} else if (mouseX < xPos[1]+5 && mouseX > xPos[1]-5 && mouseY < yPos[1]+5 && mouseY > yPos[1]-5){
 			
 			var green = color('hsba(100, 60%, 100%, 0.25)');
 			
 			fill(green);
-			
-			counter2++;
 	
 		} else if (mouseX < xPos[2]+5 && mouseX > xPos[2]-5 && mouseY < yPos[2]+5 && mouseY > yPos[2]-5){
 			
 			var green = color('hsba(150, 60%, 100%, 0.25)');
 			
 			fill(green);
-			
-			counter3++;
 	
 		} else if (mouseX < xPos[3]+5 && mouseX > xPos[3]-5 && mouseY < yPos[3]+5 && mouseY > yPos[3]-5){
 			
 			var green = color('hsba(90, 60%, 100%, 0.25)');
 			
 			fill(green);
-			
-			counter4++;
 	
 		} else if (mouseX < xPos[4]+5 && mouseX > xPos[4]-5 && mouseY < yPos[4]+5 && mouseY > yPos[4]-5){
 			
 			var green = color('hsba(100, 60%, 100%, 0.25)');
 			
 			fill(green);
-			
-			counter5++;
 	
 		}else{
 
-			
+			//blendMode(OVERLAY);
 			noFill()
-			
+			//fill(g);
 		}
-	
+	}
 	
 	triangle(xPosCurrent, yPosCurrent, xPosCurrent-10-(yPosCurrent/3),  yPosCurrent+50+(1080-yPosCurrent)/5, xPosCurrent+5+(yPosCurrent/2), yPosCurrent+(1080-yPosCurrent)/5);
 	
@@ -132,7 +122,19 @@ function draw() {
 	
 	}
 	
+	//triangle(540, 100, 500, 396, 595, 370);
+	//triangle(540, 300, 450, 556, 595, 520);
+	//triangle(540, 460, 410, 460, 775, 640);
+	//triangle(540, 600, 375, 600, 845, 745);
+	//triangle(540, 720, 345, 720, 985, 835);
 
+	
+
+
+  
+  //triangle(mouseX, mouseY, mouseX-15-(mouseY/4), mouseY+100+(1080-mouseY)/5, mouseX+5+(mouseY/2), mouseY+25+(1080-mouseY)/4);
+  
+  
 
   
 }
