54a79b1751d8a8dd4f94f840c3093d7de6f1f10d
angelawrong <46111705+angelawrong@users.noreply.github.com>
2018-12-25 09:14:38 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -129,6 +129,7 @@ function draw() {
 		}
 	
 	
+	}
 	triangle(xPosCurrent, yPosCurrent, xPosCurrent-10-(yPosCurrent/3),  yPosCurrent+50+(1080-yPosCurrent)/5, xPosCurrent+5+(yPosCurrent/2), yPosCurrent+(1080-yPosCurrent)/5);
 	
 	
